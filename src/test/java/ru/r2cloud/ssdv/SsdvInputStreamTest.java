8a5b0dff2314099903f27f8bdd868662419f786b
dernasherbrezon <rodionovamp@mail.ru>
2020-02-22 15:46:59 +0000

    a bit of tests

--- src/test/java/ru/r2cloud/ssdv/SsdvInputStreamTest.java
+++ src/test/java/ru/r2cloud/ssdv/SsdvInputStreamTest.java
@@ -1,7 +1,9 @@
 package ru.r2cloud.ssdv;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
+import java.io.ByteArrayInputStream;
 import java.util.NoSuchElementException;
 
 import org.junit.Test;
@@ -23,6 +25,13 @@ public class SsdvInputStreamTest {
 	public void testIllegalToCallNextFirst() throws Exception {
 		try (SsdvInputStream is = new SsdvInputStream(SsdvInputStreamTest.class.getClassLoader().getResourceAsStream("jy1sat.bin"), 189)) {
 			is.next();
+		}
+	}
+
+	@Test
+	public void testEof() throws Exception {
+		try (SsdvInputStream is = new SsdvInputStream(new ByteArrayInputStream(new byte[] { 0x55, 0x66, 0x0, 0x0 }))) {
+			assertFalse(is.hasNext());
 		}
 	}
 
